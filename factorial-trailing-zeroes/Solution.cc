76690d0ae6712b72b2157e28f9d1045a2e1bd995
wangyi717 <wangyi717@gmail.com>
2015-03-03 17:20:43 +0800

    add a new solution

---
+++ factorial-trailing-zeroes/Solution.cc
@@ -0,0 +1,19 @@
+/**
+ * Solution.cc
+ * @author 王毅 <wangyi717@gmail.com>
+ */
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        while (n) {
+            n /= 5;
+            ans += n;
+        }
+        return ans;
+    }
+};
